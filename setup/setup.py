1dd355e0f29012cf7b76128d2d515d7810ff591e
Jacob Jeffers <jakej@jaketheogre.com>
2017-08-13 13:45:51 -0700

    added instructions for finding storage pool for base image installation

--- setup/setup.py
+++ setup/setup.py
@@ -14,9 +14,23 @@ if virt == "" or phpv == "":
   print "Dependency Check failed!"
   print ""
   print "Please make sure libvirt and PHP7.0 are installed"
+  exit(1)
 else:
   print ""
   print "Check Passed! Moving on"
+print "	The VM storage pool is where the images that libvirt"
+print "	creates. If you are unsure run the command:"
+print "		$: virsh pool-list"
+print "	This will list the current storage pools. If there is"
+print "	only one run the following command and replace <pool>"
+print "	with the name of the storage pool"
+print "		$: virsh pool-xmldump <pool> | grep path"
+print "	This will output the path to the storage pool. You"
+print "	can copy and paste it here."
+print ""
+raw_input("press enter when ready.")
+vm_space = raw_input("Where is your VM storage pool: ")
+print "Making Directory stucture"
 raw_input()
 system('clear')
 print "all done"
