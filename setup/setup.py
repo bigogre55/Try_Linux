b488a6c31a3d8b5119632ec6e38628516990e49a
Jacob Jeffers <jakej@jaketheogre.com>
2017-08-13 23:00:16 -0700

    created folder structure and got root access and moved files

--- setup/setup.py
+++ setup/setup.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python
 
-from os import system
+from os import system,path,listdir
+from shutil import move,copy
 
+def space(n):
+  for i in range(n):
+    print ""
+
+system('clear')
+space(1)
 print "This is the setup program for Try_Linux!"
-print ""
+space(1)
+print "	This setup program needs root access to install"
+print "	and setup all necissary files."
+print "	You will be promted for admin privileges now."
+space(2)
 raw_input("Press Enter to continue.")
+system('sudo cat /dev/null')
 system('clear')
 print "Checking dependencies..."
 virt = system('which virsh')
-phpv = system('which php')
-if virt == "" or phpv == "":
-  print ""
-  print "Dependency Check failed!"
-  print ""
-  print "Please make sure libvirt and PHP7.0 are installed"
+php = system('which php')
+if virt == "" or php == "":
+  space(5)
+  print "	Dependency Check failed!"
+  space(2)
+  print "	Please make sure libvirt and PHP7.0 are installed"
+  space(2)
   exit(1)
 else:
-  print ""
-  print "Check Passed! Moving on"
+  space(5)
+  print "	Check Passed! Moving on"
+space(1)
+raw_input("Press Enter when you're ready to begin!")
+system('clear')
+space(5)
 print "	The VM storage pool is where the images that libvirt"
 print "	creates. If you are unsure run the command:"
 print "		$: virsh pool-list"
@@ -27,11 +44,32 @@ print "	with the name of the storage pool"
 print "		$: virsh pool-xmldump <pool> | grep path"
 print "	This will output the path to the storage pool. You"
 print "	can copy and paste it here."
-print ""
-raw_input("press enter when ready.")
+space(1)
 vm_space = raw_input("Where is your VM storage pool: ")
+exist_check = path.exists(vm_space)
+dir_check = path.isdir(vm_space)
+if exist_check == False:
+  space(2)
+  print "	That directory does not exist."
+  space(2)
+  exit(1)
+elif dir_check == False:
+  space(2)
+  print "	That is not a directory"
+  space(2)
+  exit(1)
 print "Making Directory stucture"
-raw_input()
+print vm_space[len(vm_space) - 1]
+if vm_space[len(vm_space) - 1] != "/":
+  vm_space += "/"
+if not path.isdir(vm_space + '../config.d'):
+  system('sudo mkdir ' + vm_space + '../config.d')
+if not path.isdir(vm_space + '../MID'):
+  system('sudo mkdir ' + vm_space + '../MID')
+raw_input('Completed Sucsessfully! Press Enter to continue')
+copy('../vm_space/recycle.sh', vm_space + '../')
 system('clear')
-print "all done"
+space(2)
+print "	all done"
+space(2)
 
