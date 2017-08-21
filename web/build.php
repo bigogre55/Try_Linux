6d4c8d5f7042c16250bfb58cb1bee067a53104d3
Jacob Jeffers <jakej@jaketheogre.com>
2017-08-21 16:43:33 -0700

    softer

--- web/build.php
+++ web/build.php
@@ -5,11 +5,11 @@ $dist = $_GET['dist'];
 $host = $_SERVER['host'];
 shell_exec("./start.sh $name $dist");
 $new_build = shell_exec("php domain_build.php $name");
-file_put_contents("$dir/.Try_Linux/config.d/$name.xml",$new_build);
-shell_exec("virsh create $dir/.Try_Linux/config.d/$name.xml > /dev/null 2>&1");
+file_put_contents("$dir.Try_Linux/config.d/$name.xml",$new_build);
+shell_exec("virsh create $dir.Try_Linux/config.d/$name.xml > /dev/null 2>&1");
 $vnc = shell_exec("virsh vncdisplay $name");
 $url = shell_exec("./url_build.sh $vnc");
 $date = date("Ymd-H:i:s");
-shell_exec("touch $dir/.Try_Linux/MID/$name-$date.mid");
+shell_exec("touch $dir.Try_Linux/MID/$name-$date.mid");
 header("Location: http://".$host.$url);
 ?>
